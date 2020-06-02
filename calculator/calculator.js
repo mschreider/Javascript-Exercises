ccd37d2bc0310a6bdce4636f6641ca29bc94fff6
Matt Schreider <matt.schreider@gmail.com>
2020-06-02 15:20:52 -0400

    completed calculator and palindrome exercises

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
-function add () {
-	
+function add(x,y) {
+	return (x+y);
 }
 
-function subtract () {
-	
+function subtract(x,y) {
+	return(x-y);
 }
 
-function sum () {
-	
+function sum(arr) {
+	let sum=0;
+	for(let x = 0; x<arr.length; x++){
+		sum += arr[x];
+	}
+	return sum;
 }
 
-function multiply () {
-	
+function multiply(arr) {
+	let product = 1;
+	for(let x = 0; x<arr.length; x++){
+		product *= arr[x];
+	}
+	return product;
 }
 
-function power() {
-	
+function power(x,y) {
+	return(x**y);
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if (num < 0) {
+    	return -1;
+	}
+	else if (num == 0) {
+	  	return 1;
+	}
+  	else {
+      	return (num * factorial(num - 1));
+  	}
 }
 
 module.exports = {
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function() {
-
+function palindromes(phrase) {
+    let phrase_reverse = "";
+    for(let x = phrase.length -1; x>=0; x--){
+        if( phrase[x].toUpperCase() != phrase[x].toLowerCase() ){  
+          phrase_reverse += phrase[x];
+        }
+        else{
+          phrase = phrase.replace(phrase[x],'');
+        }
+    }
+    if(phrase_reverse.toLowerCase() == phrase.toLowerCase()){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 module.exports = palindromes
